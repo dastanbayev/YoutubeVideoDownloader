4401e28f70aaa24e0cccaf73a2423336b187d75c
Dastanbayev02 <dastanbayev02s@gmail.com>
2022-08-13 08:10:07 +0500

    Added desktop app

---
+++ YouTubeDl.Desktop/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace YouTubeDl.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFolder_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
